a2705b67f7fdc588edb577c2ee9a1cd0c16133e5
witemirlo <jacoboberdugo@gmail.com>
2025-06-06 18:13:21 +0200

    starting parsing and fix incomlete ft_strcmp

--- src/main.c
+++ src/main.c
@@ -3,15 +3,28 @@
 int main(int argc, char* argv[])
 {
 	t_connection_data data = {0};
-	const char* addr;
+	const char* addr = NULL;
 
 	for (int i = 1; i < argc; i++) {
 		if (ft_strcmp(argv[i], "--help") == 0) {
-			printf("TODO: help\n"); // TODO: poner el outuput que toca
+			printf(
+				"Usage:\n"
+				"  %s host\n"
+				"Options:\n"
+				"  --help                      Read this help and exit\n"
+				"\n"
+				"Arguments:\n"
+				"+     host          The host to traceroute to\n"
+				, __progname
+			);
 			return EXIT_SUCCESS;
 		}
-		addr = argv[i];
-		break;
+		if (addr == NULL)
+			addr = argv[i];
+		else {
+			fprintf(stderr, "%s: Error: Bad option `%s'\n", __progname, argv[i]);
+			return EXIT_FAILURE;
+		}
 	}
 
 	get_connection_data(&data, addr);
--- src/utils.c
+++ src/utils.c
@@ -1,9 +1,11 @@
 int ft_strcmp(const char *s1, const char *s2)
 {
-        while (*s1 != '\0' && *s2 != '\0') {
-                s1++;
-                s2++;
-        }
+	while (*s1 != '\0' && *s2 != '\0') {
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
 
-        return *s1 - *s2;
+	return *s1 - *s2;
 }
