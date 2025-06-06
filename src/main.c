c8157c48bdbced9f8a4b0d70f6923d6bab65f948
witemirlo <jacoboberdugo@gmail.com>
2025-06-06 17:08:52 +0200

    use getaddrinfo and socket as receiver

--- src/main.c
+++ src/main.c
@@ -2,28 +2,42 @@
 
 int main(int argc, char* argv[])
 {
-	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	struct addrinfo hints = {0};
+	struct addrinfo *result, *rp;
 
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags = AI_CANONNAME;
+	hints.ai_protocol = 0; 
+
+	int ret = getaddrinfo(argv[1], NULL, &hints, &result);
+	if (ret != 0) {
+		fprintf(stderr, "%s:%d: %s: %s\n", __FILE__, __LINE__, __progname, gai_strerror(ret));
+		return EXIT_FAILURE;
+	}
+
+	int sockfd;
+	for (rp = result; rp != NULL; rp = rp->ai_next) {
+		sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+		if (sockfd < 0)
+			continue;
+		break;
+	}
+
+	freeaddrinfo(result);
 	if (sockfd < 0) {
+		fprintf(stderr, "%s:%d: %s: Could not connect\n", __FILE__, __LINE__, __progname);
+		return EXIT_FAILURE;
+	}
+
+	int32_t ttl = 1;
+	if (setsockopt(sockfd, IPPROTO_IP, IP_RECVERR, &ttl, sizeof(ttl)) < 0) {
 		fprintf(stderr, "%s:%d: %s: %s\n", __FILE__, __LINE__, __progname, strerror(errno));
 		return EXIT_FAILURE;
 	}
 
-	int ip = (72 << 24) | (14 << 16) | (219 << 8) | (20);
-	struct sockaddr_in addr = {
-		.sin_family = AF_INET,
-		.sin_addr.s_addr = ip,
-		.sin_port = 8080,
-		.sin_zero = 0
-	};
-
-	int32_t ttl = 2;
 	if (setsockopt(sockfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) < 0) {
-		fprintf(stderr, "%s:%d: %s: %s\n", __FILE__, __LINE__, __progname, strerror(errno));
-		return EXIT_FAILURE;
-	}
-
-	if (sendto(sockfd, "hi", 2, 0, (struct sockaddr*)&addr, sizeof(addr)) <= 0) {
 		fprintf(stderr, "%s:%d: %s: %s\n", __FILE__, __LINE__, __progname, strerror(errno));
 		return EXIT_FAILURE;
 	}
